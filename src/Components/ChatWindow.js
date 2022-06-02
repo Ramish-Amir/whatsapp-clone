8a5e9ab6e2f37100a4ed934790e9597b83185ff8
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2022-06-03 00:24:28 +0500

    Added chat window header and bottom input bar

--- src/Components/ChatWindow.js
+++ src/Components/ChatWindow.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import styles from '../styles/ChatWindow.module.css'
+import ChatWindowHeader from './ChatWindowHeader'
+import { MdEmojiEmotions, MdAttachFile, MdMic  } from "react-icons/md";
+
 
 function ChatWindow() {
   return (
-    <div className={styles.chatWindow}>ChatWindow</div>
+    <div className={styles.chatWindow}>
+        <ChatWindowHeader />
+        
+        <div className={styles.bottomInputBar}>
+            <MdEmojiEmotions className={styles.bottomIcons} />
+            <MdAttachFile className={styles.bottomIcons} />
+            <input placeholder='Type a message' />
+            <MdMic className={styles.bottomIcons} />
+
+        </div>
+
+    </div>
   )
 }
 
